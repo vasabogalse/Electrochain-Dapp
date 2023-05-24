b4267de9a94765705f5d0e0ef4cd695d2290758a
vasabogalse <vsabogalserrano@gmail.com>
2023-05-23 23:27:55 -0500

    react dashboard

--- Dapp/dashboard/src/main.js
+++ Dapp/dashboard/src/main.js
@@ -11,23 +11,41 @@
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
  */
-import Vue from "vue";
+import {Vue, h} from "vue";
 import VueRouter from "vue-router";
 import RouterPrefetch from 'vue-router-prefetch'
 import App from "./App";
 // TIP: change to import router from "./router/starterRouter"; to start with a clean layout
 import router from "./router/index";
 import "vue-connect-wallet/dist/style.css";
+import VueConnectWallet from "vue-connect-wallet";
 import BlackDashboard from "./plugins/blackDashboard";
-import i18n from "./i18n"
+// import i18n from "./i18n"
 import './registerServiceWorker'
-Vue.use(BlackDashboard);
-Vue.use(VueRouter);
-Vue.use(RouterPrefetch);
+// Vue.use(BlackDashboard);
+// Vue.use(VueRouter);
+// Vue.use(RouterPrefetch);
+//Vue.use(VueConnectWallet);
+
 
 /* eslint-disable no-new */
-new Vue({
+// new Vue({
+//   router,
+//   render: h => h(App)
+// }).$mount("#app"); 
+const { createApp } = Vue
+
+const app = createApp({
   router,
-  i18n,
-  render: h => h(App)
-}).$mount("#app");
+})
+
+app.use(BlackDashboard);
+app.use(VueRouter);
+app.use(RouterPrefetch);
+
+app.mount('#app')
+export default {
+  render() {
+    return h('div')
+  }
+}
